085bae7406c04db7f6e06d5621c33e9f4a680522
Aki.SHIMO <fezrestia@gmail.com>
2021-01-02 23:39:16 +0900

    ADD: Set web frame grip exclusion for global navigation edge gesture

---
+++ WebViewWindow/src/main/java/com/fezrestia/android/webviewwindow/view/BlockNavigationImageView.kt
@@ -0,0 +1,29 @@
+package com.fezrestia.android.webviewwindow.view
+
+import android.content.Context
+import android.graphics.Rect
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+/**
+ * ImageView for blocking edge navigation gesture trigger.
+ */
+class BlockNavigationImageView(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyle: Int) : AppCompatImageView(context, attrs, defStyle) {
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+
+    private val navExclusionRect = Rect()
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            navExclusionRect.set(left, top, right, bottom)
+            val list = listOf<Rect>(navExclusionRect)
+            this.systemGestureExclusionRects = list
+        }
+    }
+}
